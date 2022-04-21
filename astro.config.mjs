c5fad6727e8deb7b58578f4107d3f0bc2615b184
Ben <ben.mccolm-pickford@teachstarter.com>
2022-04-21 22:46:21 +1000

    upgrade fixes

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://benpickford.me",
+  integrations: [
+    sitemap(),
+    partytown({
+      forward: ["dataLayer.push"],
+    }),
+  ],
+});
